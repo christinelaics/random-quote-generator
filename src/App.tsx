7f5a5290d67a2b1cde7f541fad64e1c87157b76d
christinelaics <christinelai.cs@gmail.com>
2025-09-15 23:08:01 -0400

    rendered first quote from data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import {QUOTES} from "./data/quotes";
 
 export default function App() {
+  const first = QUOTES[0];
+
   return (
     <main>
       <h1>Quote of the Day</h1>
-      <p>Loading initial app</p>
+
+      <section>
+        <blockquote>
+          <p>{first.text}</p>
+          {first.author && <footer>- {first.author}</footer>}
+        </blockquote>
+      </section>
     </main>
-  )
+  );
 }
